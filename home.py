cfca766864b1e84bff4a7402e860eb5177ad9f66
bikaaaaaaa <96994774+bikaaaaaaa@users.noreply.github.com>
2024-05-29 11:24:35 +0200

    Update home.py

--- home.py
+++ home.py
@@ -10,35 +10,61 @@ st.set_page_config(
     initial_sidebar_state="expanded",  # Standardzustand der Seitenleiste
 )
 
-# Definition der Farben
-class Theme:
-    primaryColor = "#05f1c9"
-    backgroundColor = "#c8e3e8"
-    secondaryBackgroundColor = "#82bad2"
-    textColor = "#0f1212"
+def main():
 
-theme = Theme()
+    # Definition der Farben
+    class Theme:
+        primaryColor = "#05f1c9"
+        backgroundColor = "#c8e3e8"
+        secondaryBackgroundColor = "#82bad2"
+        textColor = "#0f1212"
 
-# Anwendung des Themes
-st.markdown(
-    f"""
-    <style>
-    .reportview-container.main.block-container {{
-        color: {theme.primaryColor};
-        background-color: {theme.backgroundColor};
-        font-family: Arial, sans-serif;
-    }}
-    .reportview-container.main {{
-        color: {theme.primaryColor};
-        background-color: {theme.backgroundColor};
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+    theme = Theme()
 
-if 'page' not in st.session_state:
-    st.session_state.page = 'Home'
+    # Anwendung des Themes
+    st.markdown(
+        f"""
+        <style>
+        .reportview-container.main.block-container {{
+            color: {theme.primaryColor};
+            background-color: {theme.backgroundColor};
+            font-family: Arial, sans-serif;
+        }}
+        .reportview-container.main {{
+            color: {theme.primaryColor};
+            background-color: {theme.backgroundColor};
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    if 'page' not in st.session_state:
+        st.session_state.page = 'Home'
+
+    # Liste der motivierenden Zitate
+    quotes = [
+        "Your chemistry teacher WILL find you and lecture you, if you still dont know how H2O is loaded.",
+        "Drink water, your organs are failing!",
+        "I 🐝 🍃 in you!!",
+        "JUST DO IT!",
+        "🦄",
+        "Never back down never what? – never give up – NEVER BACK DOWN NEVER WHAT? – Never give up!!",
+        "„Do or do not. There is no try.“ – Yoda",
+        "Never gonna give you up 🕺 Never gonna let you down 💃 Never gonna run around and desert you 🕺Never gonna make you cry 💃 Never gonna say goodbye 🕺Never gonna tell a lie and hurt you 💃",
+        "„It is not our abilities that show what we truly are … it is our choices.“ – Dumbledore",
+        "“Just keep swimming. Just keep swimming. Just keep swimming, swimming, swimming. What do we do? We swim, swim.” – Dory",
+        "You dropped this 👑",
+        "In this world you either crank that soulja boy or it cranks you",
+        "Sorry i’m too thicc to do anything half-assed",
+        "Do literally whatever makes you happy",
+        "You know what it takes to make a star shine? A shitload of fucking darkness",
+        "Be kind to ya mind",
+        "Don‘t let idiots ruin your day",
+        "Don‘t be ashamed of who you are. That’s your parents' job",
+        "Don’t be a whiny little shit",
+        "Logic will get you from A to B. Imagination will take you everywhere – Albert Einstein"
+    ]
 
 # Funktionen zur Anzeige der verschiedenen Seiten
 def show_home():
@@ -47,23 +73,31 @@ def show_home():
 
     with col1:
         if st.button("Pathways", key='pathways_button'):
-            st.session_state.page = 'Pathways'
+            st.switch_page('pages/pathways.py')
         st.image("images/pathways.jpeg", caption="Pathways")
 
     with col2:
         if st.button("Eselsbrücken", key='eselsbrücken_button'):
-            st.session_state.page = 'Eselsbrücken'
+            st.switch_page('pages/eselsbrücken.py')
         st.image("images/eselsbrücke.jpeg", caption="Eselsbrücken")
 
     with col3:
         if st.button("Memes", key='memes_button'):
-            st.session_state.page = 'Memes'
+            st.switch_page('pages/memes.py')
         st.image("images/memes.jpeg", caption="Memes")
 
     with col4:
         if st.button("Quiz", key='quiz_button'):
-            st.session_state.page = 'Quiz'
+            st.switch_page('pages/quiz.py')
         st.image("images/quiz.jpeg", caption="Quiz")
+        
+    # Zitat des Tages, zentriert und gestaltet
+    quote = get_daily_quote()
+    st.markdown(f"""
+        <div style="background-color: #e0f7fa; padding: 20px; border-radius: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); margin: 20px;">
+            <h2 style="text-align: center; color: #00796b; font-family: 'Arial', sans-serif;">{quote}</h2>
+        </div>
+    """, unsafe_allow_html=True)
 
 def show_pathways():
     st.title("Pathways")
@@ -81,55 +115,10 @@ def show_quiz():
     st.title("Quiz")
     st.write("Hier sind die Inhalte zu Quiz.")
 
-# Liste der motivierenden Zitate
-quotes = [
-    "Your chemistry teacher WILL find you and lecture you, if you still dont know how H2O is loaded.",
-    "Drink water, your organs are failing!",
-    "I 🐝 🍃 in you!!",
-    "JUST DO IT!",
-    "🦄",
-    "Never back down never what? – never give up – NEVER BACK DOWN NEVER WHAT? – Never give up!!",
-    "„Do or do not. There is no try.“ – Yoda",
-    "Never gonna give you up 🕺 Never gonna let you down 💃 Never gonna run around and desert you 🕺Never gonna make you cry 💃 Never gonna say goodbye 🕺Never gonna tell a lie and hurt you 💃",
-    "„It is not our abilities that show what we truly are … it is our choices.“ – Dumbledore",
-    "“Just keep swimming. Just keep swimming. Just keep swimming, swimming, swimming. What do we do? We swim, swim.” – Dory",
-    "You dropped this 👑",
-    "In this world you either crank that soulja boy or it cranks you",
-    "Sorry i’m too thicc to do anything half-assed",
-    "Do literally whatever makes you happy",
-    "You know what it takes to make a star shine? A shitload of fucking darkness",
-    "Be kind to ya mind",
-    "Don‘t let idiots ruin your day",
-    "Don‘t be ashamed of who you are. That’s your parents' job",
-    "Don’t be a whiny little shit",
-    "Logic will get you from A to B. Imagination will take you everywhere – Albert Einstein"
-]
-
 # Abrufen eines zufälligen Zitats, das für einen Tag zwischengespeichert wird
 @st.cache_data(ttl=86400)
 def get_daily_quote():
     return random.choice(quotes)
-
-def main():
-    # Anzeige der entsprechenden Seite
-    if st.session_state.page == 'Home':
-        show_home()
-    elif st.session_state.page == 'Pathways':
-        show_pathways()
-    elif st.session_state.page == 'Eselsbrücken':
-        show_eselsbrücken()
-    elif st.session_state.page == 'Memes':
-        show_memes()
-    elif st.session_state.page == 'Quiz':
-        show_quiz()
-
-    # Zitat des Tages, zentriert und gestaltet
-    quote = get_daily_quote()
-    st.markdown(f"""
-        <div style="background-color: #e0f7fa; padding: 20px; border-radius: 15px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); margin: 20px;">
-            <h2 style="text-align: center; color: #00796b; font-family: 'Arial', sans-serif;">{quote}</h2>
-        </div>
-    """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
